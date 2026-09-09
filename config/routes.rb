af717deaa6df71f34a1c3450a316519e600dd917
Javier Nicolás Pérez Mesa <mesafilms@gmail.com>
2026-09-09 12:10:40 +0200

    Implement user association for posts and update views for member-only access

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
+  root to: 'posts#index'
+  resources :posts, only: [:index, :create]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
