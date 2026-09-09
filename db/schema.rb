f35433a2fa5789c7d1b98a97b83bb6ed1f26d5fe
Javier Nicolás Pérez Mesa <mesafilms@gmail.com>
2026-09-09 12:26:28 +0200

    Add name and surname fields to users and update views for member greetings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_09_090857) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_09_102120) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -24,9 +24,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_09_090857) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.string "surname"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
